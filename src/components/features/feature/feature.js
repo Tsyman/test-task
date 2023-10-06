3254a5e79adbcbba09e9eee0bb6301dc93f38c6d
Maxim Tsymanovich <m.tsymanovich@syberry.com>
2023-10-06 16:51:14 +0300

    fixes

--- src/components/features/feature/feature.js
+++ src/components/features/feature/feature.js
@@ -1,8 +1,8 @@
 import './feature.styles.css';
 
-const Feature = ({title, image}) => (`
+const Feature = ({title, imageClassName}) => (`
   <div class="feature">
-    <img class="image" src=${image} alt="Feature">
+    <div class="feature__${imageClassName}"></div>
     <h6 class="feature__text">${title}</h6>
   </div>
 `);
--- src/components/features/features.js
+++ src/components/features/features.js
@@ -1,23 +1,20 @@
 import i18next from 'i18next';
 import Feature from './feature/feature';
-import unlimitedArtCreationImage from '../../assets/images/unlimited-art-creation.png';
-import exclusiveStylesImage from '../../assets/images/exclusive-styles.png';
-import magicAvatarsImage from '../../assets/images/magic-avatars.png';
 import './features.styles.css';
 
 const Features = () => {
   const featuresData = [
     {
       title: i18next.t('Unlimited Art <br>Creation'),
-      image: unlimitedArtCreationImage
+      imageClassName: 'unlimited-art-creation'
     },
     {
       title: i18next.t('Exclusive <br>Styles'),
-      image: exclusiveStylesImage
+      imageClassName: 'exclusive-styles'
     },
     {
       title: i18next.t('Magic Avatars <br>With 20% Off'),
-      image: magicAvatarsImage
+      imageClassName: 'magic-avatars'
     }
   ];
 
